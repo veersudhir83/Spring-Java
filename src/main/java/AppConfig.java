775cd1b9793c1572cc8455e7c2523df93a7ed819
veersudhir83 <veersudhir83@gmail.com>
2017-10-11 21:14:01 +0530

    Using constructor Injection

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -14,10 +14,10 @@ public class AppConfig {
 	@Bean(name = "customerService")
 	public CustomerService getCustomerService() {
 		// using Constructor injection
-		// CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
+		CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
 		
 		// using Setter injection
-		CustomerServiceImpl service = new CustomerServiceImpl();
+		// CustomerServiceImpl service = new CustomerServiceImpl();
 		// service.setCustomerRepository(getCustomerRepository());
 		return service;
 	}
--- src/main/java/io/sudheer/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/io/sudheer/pluralsight/service/CustomerServiceImpl.java
@@ -8,19 +8,15 @@ import org.springframework.stereotype.Service;
 import io.sudheer.pluralsight.model.Customer;
 import io.sudheer.pluralsight.repository.CustomerRepository;
 
-//@Service("customerService")
+//@Service("custsomerService")
 public class CustomerServiceImpl implements CustomerService {
 	
 	
 	private CustomerRepository customerRepository;
 	
 	public CustomerServiceImpl(CustomerRepository customerRepository) {
-		System.out.println("We are using Constructor Injection");
+		System.out.println("CustomerService: We are using Constructor Injection");
 		this.customerRepository = customerRepository;
-	}
-	
-	public CustomerServiceImpl() {
-		
 	}
 
 	@Override
@@ -28,9 +24,9 @@ public class CustomerServiceImpl implements CustomerService {
 		return customerRepository.findAll();
 	}
 
-	@Autowired
+	//@Autowired
 	public void setCustomerRepository(CustomerRepository customerRepository) {
-		System.out.println("We are using Setter Injection");
+		System.out.println("CustomerService: We are using Setter Injection");
 		this.customerRepository = customerRepository;
 	}
 
